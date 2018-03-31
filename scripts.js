1a4ca73d4a1ed0428b816bb21f4e51b2a69496d7
Goska <36965568+goskanowak@users.noreply.github.com>
2018-03-31 02:35:56 +0200

    Delete comment

--- scripts.js
+++ scripts.js
@@ -1,15 +1,10 @@
 function drawTree (height) {
     for (var i = 1; i < height; i++) {
         var star = '*';
-            for (var j = 1; j < i /* *2-1 ( zmienia wartość choinki o 2 gwiazdki*/; j++) {
-                
+            for (var j = 1; j < i; j++) {
                   star += '*';
-             
     }
-    console.log(star);
-       
+    console.log(star); 
     }
 }
-// document.getElementById('tree').innerHTML = star;
-// jak zrobić aby choinka wyświetlała się na stronie, a nie tylko w konsoli?
-    drawTree(10);
+drawTree(10);
